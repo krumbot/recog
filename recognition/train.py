d493de4f719b15f26d42b0e863de6fe13c9db392
Vikrum Joshi <vikrum.joshi@gmail.com>
2018-01-06 13:55:50 -0500

    with iterative predictions implemented

--- recognition/train.py
+++ recognition/train.py
@@ -13,13 +13,15 @@ def split_train_test(image_list):
     split_value = math.ceil(len(image_list) * TRAIN_PERCENTAGE / 100)
     return image_list[:split_value], image_list[split_value:]
 
-def get_lowest_confidence(predictions):
+def get_lowest_confidence(predictions, target):
     lowest = 0
     idx = -1
-    for index, prediction, confidence in enumerate(predictions):
-        if confidence < lowest or if lowest == 0:
+    index = 0
+    for prediction, confidence in predictions:
+        if (confidence < lowest or lowest == 0) and prediction == target:
             lowest = confidence
             idx = index
+        index += 1
     return confidence, idx
 
 def predict(recognizer, prediction_set):
@@ -28,7 +30,7 @@ def predict(recognizer, prediction_set):
         predictions.append(recognizer.predict(prediction_image))
     return predictions
 
-def train_for_person(path, prev_recognizer=None):
+def train_for_person(path, recognizer_data=None):
     images, name = ps.segment_into_n_face_list(path)
     images.sort(key=len)
     person_number = get_person_number()
@@ -39,19 +41,21 @@ def train_for_person(path, prev_recognizer=None):
     face_recognizer = cv2.face.LBPHFaceRecognizer_create()
     trained = False
 
-    if prev_recognizer is not None:
-        face_recognizer.read(prev_recognizer)
+    if recognizer_data is not None:
+        face_recognizer.read(recognizer_data)
         trained = True
     current_segment = 1
     faces_of_interest = []
 
     for face_segment in images:
         if len(face_segment) == current_segment:
-            faces_of_interest += face_segment
+            faces_of_interest.append(face_segment)
         else:
             if current_segment == 1:
-                training_set, testing_set = split_train_test(faces_of_interest)
+                flattened_foi = [face[0] for face in faces_of_interest]
+                training_set, testing_set = split_train_test(flattened_foi)
                 labels = np.array([person_number] * len(training_set))
+                super_test_set += testing_set
                 if not trained:
                     face_recognizer.train(training_set, labels)
                     trained = True
@@ -59,12 +63,18 @@ def train_for_person(path, prev_recognizer=None):
                     face_recognizer.update(training_set, labels)
                 super_test_set += testing_set
             else:
-                predictions = predict(face_recognizer, faces_of_interest)
-                conf, idx = get_lowest_confidence(predictions)
-
-
+                foi = []
+                for face_segment in faces_of_interest:
+                    predictions = predict(face_recognizer, face_segment)
+                    conf, idx = get_lowest_confidence(predictions, person_number)
+                    foi.append(face_segment[idx])
+                training_set, testing_set = split_train_test(foi)
+                labels = np.array([person_number] * len(training_set))
+                super_test_set += testing_set                
+                face_recognizer.update(training_set, labels)
             current_segment = len(face_segment)
             faces_of_interest = []
     return face_recognizer
 
+train_for_person('./assets/actors/AdamDriver')
 
